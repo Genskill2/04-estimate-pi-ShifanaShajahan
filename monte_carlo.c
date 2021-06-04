155d84c1bb4e67ced0163652803f0665e313fbf0
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-06-04 16:32:48 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -4,20 +4,39 @@
 #include <stdio.h>
 #include <math.h>
 int main(viod){
-
-float mc_pi(int n){
-    int circle = 0;
-    int square = 0;
-    float x, y;
-for(int i=1; i<=n; i++){
-    x= frandom();
-    y= frandom();
-    if (((xx)+ (yy))<=1){circle++;}
-    else {square++;}
-} square = square + circle;
- float pi = 4*circle/square;
-return pi;
+double pi(double tolerance)
+{
+	double x, y, val, error;
+	unsigned long sampled = 0, hit = 0, i;
+ 
+	do {
+		/* don't check error every turn, make loop tight */
+		for (i = 1000000; i; i--, sampled++) {
+			x = rand() / (RAND_MAX + 1.0);
+			y = rand() / (RAND_MAX + 1.0);
+			if (x * x + y * y < 1) hit ++;
+		}
+ 
+		val = (double) hit / sampled;
+		error = sqrt(val * (1 - val) / sampled) * 4;
+		val *= 4;
+ 
+		/* some feedback, or user gets bored */
+		fprintf(stderr, "Pi = %f +/- %5.3e at %ldM samples.\r",
+			val, error, sampled/1000000);
+	} while (!hit || error > tolerance);
+              /* !hit is for completeness's sake; if no hit after 1M samples,
+                 your rand() is BROKEN */
+ 
+	return val;
 }
+ 
+int main()
+{
+	printf("Pi is %f\n", pi(3e-4)); /* set to 1e-4 for some fun */
+	return 0;
+}
+
 
 
 float frandom() {
