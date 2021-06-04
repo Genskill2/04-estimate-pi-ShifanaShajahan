c4fe382506bc5fc2bedecac2c49c4aa465294da7
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-06-04 14:07:59 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -1,34 +1,54 @@
-//monte_carlo.c//
+
+#include <assert.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
-#include <time.h>
-#define SEED time(NULL)
 
-int main() {
+float mc_pi(int);
 
- srand( SEED );
- int i, count, n;
- double x,y,z,pi;
+  int n;
+  float x=frandom();
+  float y=frandom();
+  float d=(pow(x,2)+ pow(y,2));
+  for (int count=0;count<=n;count++){
+       float pi=(4*count/n);
+     if (d<=1){
+           return(count);
+    }
+      else
+      {
+           abort();
+       }    
+    
+    return(pi);
 
- printf("n = ");
- scanf("%d", &n);
-
- count = 0;
-
- for(i = 0; i < n; ++i) {
-
-     x = (double)rand() / RAND_MAX;
-
-     y = (double)rand() / RAND_MAX;
-
-     z = x * x + y * y;
-
-     if( z <= 1 ) count++;
- }
- pi = (double) count / n * 4;
- 
- printf("Approximate PI = %g", pi);
-
- return(0);
+float frandom() {
+  long int q = random();
+  float ret = (float)q/(float)RAND_MAX;
+  return ret;
 }
+
+int main(void) {
+  float pi0;
+  float pi1;
+  
+  pi0 = mc_pi(25000);
+  pi1 = mc_pi(25000);
+  printf("%f %f\n", pi0, pi1);
+  
+  if (pi0 == pi1) {
+      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
+      abort();
+    }
+  if (fabs(pi0 - pi1) > 0.05) {
+      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
+      abort();
+  }
+  for (int i=2000; i<5000; i++) {
+    pi0 = mc_pi(i);
+    if (!(fabs(pi0 - M_PI) < 0.4)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
+      abort();
+    }
+    }
+    }
